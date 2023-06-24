1b94ee428fd67ff82fc270527b04fcf5d06c7623
MattBCM <matbcm01@gmail.com>
2023-06-24 15:03:39 -0400

    changed navbar to be more responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,11 +10,20 @@ const Navigation = (props) =>{
         props.handleCallBack(event.target.value)
     }
     return(
+    /*
     <ul className='navigationBar'>
         <li><Link to='/'><img src="./assets/logo-color.png" alt="" /></Link></li>
         <li><input type="text" name='search' className='searchBar' placeholder='Search...' onChange={handleMessage} value={message}/></li>
         <li><Link to='/create'><button>Create New Post</button></Link></li>
+        <li><div><img src="https://braverplayers.org/wp-content/uploads/2022/09/blank-pfp-300x300.png" alt="" /></div></li>
     </ul>
+    */
+    <div className='navigationBar'>
+        <div><Link to='/'><img src="./src/assets/logo-color.png" alt="" /></Link></div>
+        <div><input type="text" name='search' className='searchBar' placeholder='Search...' onChange={handleMessage} value={message}/></div>
+        <div><Link to='/create'><button>Create New Post</button></Link></div>
+        <div><img src="https://braverplayers.org/wp-content/uploads/2022/09/blank-pfp-300x300.png" alt="" id='pfp' /></div>
+    </div>
     )
 
 
